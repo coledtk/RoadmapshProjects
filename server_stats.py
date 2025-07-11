09e3baeb2d56b855c2be40a3314134dd63b73394
Cole Detrick <153991478+coledtk@users.noreply.github.com>
2025-07-11 06:55:47 -0500

    Update server_stats.py

--- server_stats.py
+++ server_stats.py
@@ -8,18 +8,18 @@
 import psutil 
 
 
-def print_usage():
+def get_usage():
     usage = psutil.cpu_percent(interval=1)
     return usage
 
-def print_mem_usage():
+def get_mem_usage():
     memory = psutil.virtual_memory()
     mfree = memory[4]
     mused = memory[3]
     mpercentage = memory[2]
     return mfree, mused, mpercentage
 
-def print_disk_space():
+def get_disk_space():
     space = psutil.disk_usage('/')
     dfree = space[2]
     dused = space[1]
@@ -29,3 +29,39 @@ def print_disk_space():
 def get_top_processes(sort_by='value', limit=5):
     processes = []
 
+    for i in psutil.proces_iter(['pid', 'name']):
+        try:
+            if sort_by == 'cpu':
+                usage = proc.cpu_percent(interval=0.1)
+            elif sort_by == 'mem'
+                usage = proc.memory_info().rss
+            else:
+                print("invalid")
+                return []
+            
+            processes.append({
+                'pid': proc.info['pid'],
+                'name': proc.info['name'],
+                'usage': usage
+            })
+        except error as e
+            print(f"{e}")
+            continue
+    processes.sort(key=lambda p: p['usage'], reverse=True)
+    return processes[:limit]
+
+if __name__ == '__main__':
+    usage = get_usage()
+    print(usage)
+
+    musage = get_mem_usage()
+    print(musage)
+
+    space = get_disk_space()
+    print(space)
+
+    top5cpu = get_top_processes(sort_by='cpu')
+    print(top5cpu)
+
+    top5mem = get_top_processes(sort_by='mem')
+    print(top5mem)
