9979643b8832a427811b2c35501c098fef4641d3
cole <coledetrick@gmail.com>
2025-07-12 06:40:29 -0500

    requests

--- analyze_Nginx/analyze_Nginx.py
+++ analyze_Nginx/analyze_Nginx.py
@@ -1,5 +1,5 @@
 #https://gist.githubusercontent.com/kamranahmedse/e66c3b9ea89a1a030d3b739eeeef22d0/raw/77fb3ac837a73c4f0206e78a236d885590b7ae35/nginx-access.log
 import requests
 
-response = get.requests('https://gist.githubusercontent.com/kamranahmedse/e66c3b9ea89a1a030d3b739eeeef22d0/raw/77fb3ac837a73c4f0206e78a236d885590b7ae35/nginx-access.log')
+response = requests.get('https://gist.githubusercontent.com/kamranahmedse/e66c3b9ea89a1a030d3b739eeeef22d0/raw/77fb3ac837a73c4f0206e78a236d885590b7ae35/nginx-access.log')
 print(response)
