a1124a4b064cbe4a8e532ccc3b79f5e9fb6242b0
Cole Detrick <153991478+coledtk@users.noreply.github.com>
2025-07-12 03:26:51 -0500

    bug fixes, added formatting for units of measurement.

--- server_stats.py
+++ server_stats.py
@@ -7,49 +7,60 @@
 '''
 import psutil 
 
+# formats the return value (bytes) into correct unit of measurement
+def format_bytes(size):
+    for unit in ['B', "KB", 'MB', 'GB', 'TB']:
+        if size < 1024:
+            return f"{round(size, 2)} {unit}"
+        size /= 1024
+
 # waits 1 second, then measures CPU usage over that time, returns a float.
 def get_usage():
     usage = psutil.cpu_percent(interval=1)
     return usage
 
-# 
+
+# get all virtual memory information then parses for attributes.
 def get_mem_usage():
     memory = psutil.virtual_memory()
-    mfree = memory[4]
-    mused = memory[3]
-    mpercentage = memory[2]
+    mfree = format_bytes(memory.available)
+    mused = format_bytes(memory.used)
+    mpercentage = format_bytes(memory.percent)
     return mfree, mused, mpercentage
 
+
+# 
 def get_disk_space():
     space = psutil.disk_usage('/')
-    dfree = space[2]
-    dused = space[1]
-    dpercentage = space[3]
+    dfree = format_bytes(space.free)
+    dused = format_bytes(space.used)
+    dpercentage = format_bytes(space.percent)
     return dfree, dused, dpercentage
 
-def get_top_processes(sort_by='value', limit=5):
+def get_top_processes(sort_by='cpu', limit=5):
     processes = []
 
-    for i in psutil.proces_iter(['pid', 'name']):
+    for proc in psutil.proces_iter(['pid', 'name']):
         try:
             if sort_by == 'cpu':
                 usage = proc.cpu_percent(interval=0.1)
             elif sort_by == 'mem'
                 usage = proc.memory_info().rss
             else:
-                print("invalid")
+                print("invalid sort option")
                 return []
             
             processes.append({
                 'pid': proc.info['pid'],
                 'name': proc.info['name'],
-                'usage': usage
+                'usage': format_bytes(usage)
             })
-        except error as e
-            print(f"{e}")
+        except Exception as e
+            print(f"Error: {e}")
             continue
     processes.sort(key=lambda p: p['usage'], reverse=True)
     return processes[:limit]
+
 
 if __name__ == '__main__':
     usage = get_usage()
