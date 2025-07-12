24e266164e5bcbecbb005bff7f2af2b3702d025a
Cole Detrick <153991478+coledtk@users.noreply.github.com>
2025-07-12 07:03:20 -0500

    Update analyze_Nginx.sh

--- analyze_Nginx/analyze_Nginx.sh
+++ analyze_Nginx/analyze_Nginx.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 #https://gist.githubusercontent.com/kamranahmedse/e66c3b9ea89a1a030d3b739eeeef22d0/raw/77fb3ac837a73c4f0206e78a236d885590b7ae35/nginx-access.log
+# https://www.digitalocean.com/community/tutorials/awk-command-linux-unix#printing-all-lines-in-a-file
