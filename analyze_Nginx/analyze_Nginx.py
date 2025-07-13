f05712d7c7f609a776ec54f978d9bcaddf88ad4e
cole <coledetrick@gmail.com>
2025-07-13 00:53:06 -0500

    corrected gathering of log file.

--- analyze_Nginx/analyze_Nginx.py
+++ analyze_Nginx/analyze_Nginx.py
@@ -1,5 +1,9 @@
 #https://gist.githubusercontent.com/kamranahmedse/e66c3b9ea89a1a030d3b739eeeef22d0/raw/77fb3ac837a73c4f0206e78a236d885590b7ae35/nginx-access.log
-import requests
+import os
 
-response = requests.get('https://gist.githubusercontent.com/kamranahmedse/e66c3b9ea89a1a030d3b739eeeef22d0/raw/77fb3ac837a73c4f0206e78a236d885590b7ae35/nginx-access.log')
-print(response)
+location = r'C:\Users\cdetrick\OneDrive - Sirius XM Radio Inc\Desktop\RoadmapshProjects\RoadmapshProjects\analyze_Nginx'
+for f in os.listdir(location):
+    if f == 'nginx.log':
+        fl = open(os.path.join(location, 'nginx.log'), "r")
+        print(fl.read())
+
