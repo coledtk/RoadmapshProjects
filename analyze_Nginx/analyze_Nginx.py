d0b9dc13d35cdfab3415ecf6557cf6ec84d4b3c6
cole <coledetrick@gmail.com>
2025-07-13 01:52:23 -0500

    changes

--- analyze_Nginx/analyze_Nginx.py
+++ analyze_Nginx/analyze_Nginx.py
@@ -1,9 +1,18 @@
+'''Top 5 IP addresses with the most requests
+Top 5 most requested paths
+Top 5 response status codes
+Top 5 user agents
+'''
 #https://gist.githubusercontent.com/kamranahmedse/e66c3b9ea89a1a030d3b739eeeef22d0/raw/77fb3ac837a73c4f0206e78a236d885590b7ae35/nginx-access.log
 import os
+import re
 
 location = r'C:\Users\cdetrick\OneDrive - Sirius XM Radio Inc\Desktop\RoadmapshProjects\RoadmapshProjects\analyze_Nginx'
-for f in os.listdir(location):
-    if f == 'nginx.log':
-        fl = open(os.path.join(location, 'nginx.log'), "r")
-        print(fl.read())
+for filename in os.listdir(location):
+    if filename == 'nginx.log':
+        f = open(os.path.join(location, 'nginx.log'), "r")
+        data = f.read()
 
+def get_top5_IP(data):
+    x = ''.join(re.findall("[0-9]{3}[.][(0-9]{3}|{2})]{3}"))
+    print(x)
